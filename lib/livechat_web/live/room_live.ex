fa009fb58812d5e3198a91cef1f354a1193a9523
viargentum <viargentum@riseup.net>
2021-07-18 00:13:21 -0400

    feat: add user(s) typing status

--- lib/livechat_web/live/room_live.ex
+++ lib/livechat_web/live/room_live.ex
@@ -20,13 +20,18 @@ defmodule LivechatWeb.RoomLive do
        message: "",
        messages: [],
        user_list: [],
+       typing: [],
        temporary_assigns: [messages: []]
      )}
   end
 
   @impl true
   def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
-    message = %{id: UUID.uuid4(), username: socket.assigns.username, content: message}
+    message = %{
+      id: UUID.uuid4(),
+      username: socket.assigns.username,
+      content: message
+    }
 
     LivechatWeb.Endpoint.broadcast(
       socket.assigns.topic,
@@ -39,11 +44,37 @@ defmodule LivechatWeb.RoomLive do
 
   @impl true
   def handle_event("form_update", %{"chat" => %{"message" => message}}, socket) do
+    LivechatWeb.Endpoint.broadcast(
+      socket.assigns.topic,
+      "new-typer",
+      socket.assigns.username
+    )
+
     {:noreply, assign(socket, message: message)}
   end
 
   @impl true
+  def handle_info(%{event: "new-typer", payload: typer}, socket) do
+    existing = socket.assigns.typing
+    typing = if typer in existing, do: existing, else: existing ++ [typer]
+    {:noreply, assign(socket, typing: typing)}
+  end
+
+  @impl true
+  def handle_info(%{event: "typing-update", payload: typers}, socket) do
+    {:noreply, assign(socket, typing: typers)}
+  end
+
+  @impl true
   def handle_info(%{event: "new-message", payload: message}, socket) do
+    typers = List.delete(socket.assigns.typing, socket.assigns.username)
+
+    LivechatWeb.Endpoint.broadcast(
+      socket.assigns.topic,
+      "typing-update",
+      typers
+    )
+
     {:noreply, assign(socket, messages: [message])}
   end
 
@@ -84,5 +115,26 @@ defmodule LivechatWeb.RoomLive do
     ~E"""
     <p id="<%= id %>"><strong><%= username %></strong>: <%= content %></p>
     """
+  end
+
+  def display_typing(typers) do
+    length = length(typers)
+
+    cond do
+      length == 1 ->
+        ~E"""
+        <p><%= typers %> is typing...</p>
+        """
+
+      length > 1 && length < 4 ->
+        ~E"""
+        <p><%= Enum.join(typers, " and ") %> are typing...</p>
+        """
+
+      true ->
+        ~E"""
+        <p>several people are typing...</p>
+        """
+    end
   end
 end
